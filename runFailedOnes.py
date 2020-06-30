a367fa805c72ba0e92bb1e4714c88cbb432dfb9a
dongchen-coder <jameschennerd@gmail.com>
2020-06-30 17:38:45 +0800

    update run failed script

--- runFailedOnes.py
+++ runFailedOnes.py
@@ -5,9 +5,12 @@ files = os.listdir(path)
 
 for name in files:
 	f = open(path+name, 'r')
+	flag = False
 	for line in f:
-		if ("(T^T)" in line):
-			print name.replace("_result.txt", ""), line.replace("\n","")
-			os.system("./bin/symRiSymthesiser ./inputoutput/" + name.replace("_result", "") + " > ./synResult/" + name)
+		if ("(^0^)" in line):
+			flag = True
 	f.close()
+	if (flag == False):
+		print name.replace("_result.txt", ""), line.replace("\n","")
+		os.system("./bin/symRiSymthesiser ./inputoutput/" + name.replace("_result", "") + " > ./synResult/" + name)
 
