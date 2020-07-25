60d1ff1ed636ff44a33ed5e876e15a4084e6ac63
EC2 Default User <ec2-user@ip-172-31-28-248.us-west-2.compute.internal>
2020-07-25 11:45:43 +0000

    update run script to support multiple benchmarks

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
-for entry in "./inputoutput"/*
+for bench in stencil
 do
-	name=${entry#./inputoutput/}
-	name=${name%.txt}
-	echo $name
-    ./bin/symRiSymthesiser ./inputoutput/$name.txt > ./synResult/${name}_result.txt
+	for entry in "./inputoutput/ris_per_iter_ref"/${bench}/*
+	do
+		name=${entry#./inputoutput/ris_per_iter_ref/${bench}/}
+		name=${name%.txt}
+		echo $name
+    		./bin/symRiSymthesiser ./inputoutput/ris_per_iter_ref/${bench}/$name.txt > ./synResult/${bench}/${name}_result.txt
+	done
 done
