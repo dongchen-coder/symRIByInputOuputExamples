fececdecc065e93dd4dc9cd10141f14716297174
dongchen-coder <jameschennerd@gmail.com>
2020-07-31 17:51:23 +0800

    adding summerizing python script to summerize the synthesis result into one file

---
+++ synResult/summarizeResults.py
@@ -0,0 +1,30 @@
+import os
+
+def summerizeForBench(bench):
+	print "Start to summerize " + bench
+ 
+	if (not os.path.isdir("./" + bench)):
+		return
+
+	files = os.listdir("./" + bench)
+
+	outputFile = open("./all/" + bench + ".txt", "w")
+
+	for f in files:
+		name = f
+		
+		f = open("./"+bench+"/"+f, 'r')
+		for line in f:
+			if ("(^0^)" in line):
+				prog = line.replace("Searched Program (^0^) :", "Prog:")
+				outputFile.write(name + " "+ prog)
+		f.close()
+
+	outputFile.close()
+
+	return
+
+benches = ["stencil", "2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm", "convolution_2d", "convolution_3d", "trangle"]
+
+for bench in benches:
+	summerizeForBench(bench)
