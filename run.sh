cc9441e136b75cb886c11fcda01b6b5ab822b45a
dongchen-coder <jameschennerd@gmail.com>
2020-07-31 12:35:12 +0800

    add mutiple process support to run synthesizer in parallel in run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+NPROC=0
 for bench in cholesky durbin floyd_warshall gemver gesummv lu ludcmp mvt nussinov trisolv #stencil
 do
 	mkdir ./synResult/${bench}
@@ -6,6 +7,12 @@ do
 		name=${entry#./inputoutput/ris_per_iter_ref/${bench}/}
 		name=${name%.txt}
 		echo $name
-		./bin/symRiSymthesiser ./inputoutput/ris_per_iter_ref/${bench}/$name.txt > ./synResult/${bench}/${name}_result.txt
+		./bin/symRiSymthesiser ./inputoutput/ris_per_iter_ref/${bench}/$name.txt > ./synResult/${bench}/${name}_result.txt &
+
+		NPROC=$(($NPROC+1))
+		if [ "$NPROC" -ge 2 ]; then
+			wait
+			NPROC=0
+		fi
 	done
 done
