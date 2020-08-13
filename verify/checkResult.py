b252465148deadb7f2d29d815a47251242c5d7e0
dongchen-coder <jameschennerd@gmail.com>
2020-08-14 02:24:38 +0800

    update verfiy script

--- verify/checkResult.py
+++ verify/checkResult.py
@@ -6,23 +6,77 @@ import matplotlib.pyplot as plt
 symbolicHist = {}
 
 def recordSymRI(bench, conf, dslCode):
-	conf = conf.replace(bench + "_ref_","")
-	conf = conf.split("_iter_")
-	ref_id = int(conf[0])
+	if ("_refsnk_" in conf):
+		conf = conf.replace(bench + "_refsrc_", "")
+		conf = conf.replace("_refsnk_", " ")
+		conf = conf.replace("_iter_", " ")
+		conf = conf.split(" ")
+		
+		ref_src_id = int(conf[0])
+		ref_snk_id = int(conf[1])
+		idxs_str = conf[2].split("_")
+		idxs = []
+		for idx in idxs_str:
+			idxs.append(int(idx))
+		idxs = tuple(idxs)
+	else:
+		conf = conf.replace(bench + "_refsrc_","")
+		conf = conf.replace("_iter_", " ")
+		conf = conf.split(" ")
+		ref_id = int(conf[0])
+		idxs_str = conf[1].split("_")
+		idxs = []	
+		for idx in idxs_str:
+			idxs.append(int(idx))
+		idxs = tuple(idxs)
 
-	idxs_str = conf[1].split("_")
-	idxs = []
+		if (ref_id not in symbolicHist.keys()):
+			symbolicHist[ref_id] = {}
 	
-	for idx in idxs_str:
-		idxs.append(int(idx))
-
-	idxs = tuple(idxs)
-
-	if (ref_id not in symbolicHist.keys()):
-		symbolicHist[ref_id] = {}
+		symbolicHist[ref_id][idxs] = dslCode
 	
-	symbolicHist[ref_id][idxs] = dslCode
 	return
+
+def RItoMR(riHisto):
+	maxRI = 0
+	totalNumRI = 0
+	for ri in riHisto.keys():
+		if (maxRI < ri):
+			maxRI = ri
+		totalNumRI += riHisto[ri]
+
+	accumulateNumRI = 0.0
+	
+	riSortedReverse = sorted(riHisto.keys())
+	riSortedReverse.reverse()
+	P = {}
+	for ri in riSortedReverse:
+		P[ri] = accumulateNumRI / totalNumRI
+		accumulateNumRI += riHisto[ri]
+	P[0] = 1
+	
+	sumP = 0.0
+	t = 0
+	prev_t = 0
+	MR_pred = -1
+	MR = {}
+	for c in range(maxRI+1):
+		while(sumP < c and t <= maxRI):
+			if (t in P):
+				sumP += P[t]
+				prev_t = t
+			else:
+				sumP += P[prev_t]
+			t += 1
+		if (MR_pred != -1):
+			MR[c] = P[prev_t]
+			MR_pred = P[prev_t]
+		else:
+			if (MR_pred - P[prev_t] < 0.0001):
+				MR[c] = P[prev_t]
+				MR_pred = P[prev_t]
+	
+	return MR
 
 def removeUnbalancedParenthesis(code):
 
@@ -167,18 +221,29 @@ def scaleIndexByCodeStructure(idx):
 
 	return scaled_idx
 
-def evaluate(symBoundForPred):
+def evaluate(symbolicHistToUse, symBoundForPred, method):
 
 	histogram = {}
 	
-	for ref in symbolicHist.keys():
-		for idx in symbolicHist[ref].keys():
-			scaled_idx = scaleIndexByCodeStructure(idx)	
-			ri = int(evaluateSingleCode(scaled_idx, symBoundForPred, symbolicHist[ref][idx]))
-			if (ri in histogram.keys()):
-				histogram[ri] += 1
-			else:
-				histogram[ri] = 1
+	if (method == "src"):
+		for ref in symbolicHistToUse.keys():
+			for idx in symbolicHistToUse[ref].keys():
+				scaled_idx = scaleIndexByCodeStructure(idx)	
+				ri = int(evaluateSingleCode(scaled_idx, symBoundForPred, symbolicHistToUse[ref][idx]))
+				if (ri in histogram.keys()):
+					histogram[ri] += 1
+				else:
+					histogram[ri] = 1
+	if (method == "srcsnk"):
+		for ref in symbolicHistToUse.keys():
+			for idx in symbolicHistToUse[ref].keys():
+				scaled_idx = scaleIndexByCodeStructure(idx) 
+				ri = int(evaluateSingleCode(scaled_idx, symBoundForPred, symbolicHistToUse[ref][idx]))
+				if (ri in histogram.keys()):
+					histogram[ri] += 1
+				else:
+					histogram[ri] = 1
+
 	return histogram
 		
 def readTraceFile(name, symBoundValueForPred):
@@ -200,22 +265,33 @@ def readTraceFile(name, symBoundValueForPred):
 	f.close()
 	return histogram
 
-def plotHistoToCompare(histogram_predicted, histogram_trace, bench, conf):
-	sortedRi = sorted(histogram_predicted.keys())
-	if (-1 in sortedRi): sortedRi.remove(-1)
-	if (0 in sortedRi): sortedRi.remove(0)
-	cnt = 0
-	for x in sortedRi:
-		cnt += histogram_predicted[x]
-	plt.bar(sortedRi, [float(histogram_predicted[x]) / cnt for x in sortedRi], color = 'b', width = 0.4, label = "SymbolicRI")
+def plotHistoToCompare(histogram_src_predicted, histogram_srcsnk_predicted, histogram_trace, bench, conf):
+	sortedRi = sorted(histogram_src_predicted.keys())
+	if (len(sortedRi) != 0):
+		if (-1 in sortedRi): sortedRi.remove(-1)
+		if (0 in sortedRi): sortedRi.remove(0)
+		cnt = 0
+		for x in sortedRi:
+			cnt += histogram_src_predicted[x]
+		plt.bar(sortedRi, [float(histogram_src_predicted[x]) / cnt for x in sortedRi], color = 'b', width = 0.4, alpha=0.3, label = "SymbolicRI-SRC")
+
+	sortedRi = sorted(histogram_srcsnk_predicted.keys())
+	if (len(sortedRi) != 0):
+		if (-1 in sortedRi): sortedRi.remove(-1)
+		if (0 in sortedRi): sortedRi.remove(0)
+		cnt = 0
+		for x in sortedRi:
+			cnt += histogram_srcsnk_predicted[x]
+		plt.bar(sortedRi, [float(histogram_srcsnk_predicted[x]) / cnt for x in sortedRi], color = 'b', width = 0.4, alpha=0.3, label = "SymbolicRI-SRCSNK")
 
 	sortedRi = sorted(histogram_trace.keys())
-	if (-1 in sortedRi): sortedRi.remove(-1)
-	if (0 in sortedRi): sortedRi.remove(0)
-	cnt = 0
-	for x in sortedRi:
-		cnt += histogram_trace[x]
-	plt.bar([x+0.5 for x in sortedRi], [float(histogram_trace[x]) / cnt for x in sortedRi], color = 'r', width = 0.4, label = "Trace")
+	if (len(sortedRi) != 0):
+		if (-1 in sortedRi): sortedRi.remove(-1)
+		if (0 in sortedRi): sortedRi.remove(0)
+		cnt = 0
+		for x in sortedRi:
+			cnt += histogram_trace[x]
+		plt.bar(sortedRi, [float(histogram_trace[x]) / cnt for x in sortedRi], color = 'r', width = 0.4, alpha=0.3, label = "Trace")
 	
 	plt.ylabel("Percentage")
 	plt.xlabel("Reuse intervals")
@@ -223,29 +299,47 @@ def plotHistoToCompare(histogram_predicted, histogram_trace, bench, conf):
 	plt.yscale('log')
 	plt.legend()
 	plt.title(bench + conf)
-	plt.savefig("./visualCompare/" + bench + conf + ".pdf")
+	plt.savefig("./visualCompare/" + bench + conf + "_RIHisto" + ".pdf")
 	plt.clf()
 	return
 
-def checkSingleBench(bench, path, numOfSymBounds, symBoundValueForPred):
-	
-	f = open(path, "r")	
+def plotMRToCompare(mr_src_predicted, mr_srcsnk_predicted, mr_trace, bench, conf):
 
-	succeed = 0
-	total = 0
+	plt.plot(mr_src_predicted.keys(), [mr_src_predicted[x] for x in mr_src_predicted.keys()], color = 'b', label = "SymbolibRI-SRC->MR", alpha=0.3)
+	plt.plot(mr_srcsnk_predicted.keys(), [mr_srcsnk_predicted[x] for x in mr_srcsnk_predicted.keys()], color = 'b', label = "SymbolibRI-SRCSNK->MR", alpha=0.3)
+	plt.plot(mr_trace.keys(), [mr_trace[x] for x in mr_trace.keys()], color = 'r', label = "TraceRI->MR", alpha=0.3)
+
+	plt.xlabel("Cache Size")
+	plt.ylabel("Miss Ratio")
+	plt.xscale("log")
+	plt.legend()
+	plt.title(bench + conf)
+	plt.savefig("./visualCompare/" + bench + conf + "_MR" + ".pdf")
+	plt.clf()
+	return
+
+def checkSingleBench(benchName, path, numOfSymBounds, symBoundValueForPred):
 	
-	for line in f:
+	f_src = open(path + benchName + "_src.txt", "r")	
+	f_srcsnk = open(path + benchName + "_srcsnk.txt", "r")
+
+	for line in f_src:
 		conf = line[0 : line.find(' Prog:')]
-		conf = conf.replace("_result.txt", "")	
+		conf = conf.replace("_src_result.txt", "")	
 		prog = line[line.find(' Prog:') + 7: len(line)]	
-		recordSymRI(bench, conf, prog)
-	
-	f.close()
+		recordSymRI(benchName, conf, prog)
+	f_src.close()
+	symbolicHistSrc = dict(symbolicHist)
+	symbolicHist.clear()
 
-	if (total != 0):
-		print "total", total, "succeed", succeed, float(succeed)/total, "failed", total-succeed, float(total-succeed)/total
-	else:
-		print "No Symbolic RI"
+	for line in f_srcsnk:
+		conf = line[0 : line.find(' Prog:')]
+		conf = conf.replace("_srcsnk_result.txt", "")  
+		prog = line[line.find(' Prog:') + 7: len(line)]
+		recordSymRI(benchName, conf, prog)
+	f_srcsnk.close()
+	symbolicHistSrcsnk = symbolicHist
+	symbolicHist.clear()
 
 	confs = []
 
@@ -261,8 +355,6 @@ def checkSingleBench(bench, path, numOfSymBounds, symBoundValueForPred):
 				for bound in symBoundValueForPred:
 					confs_new.append(conf + "_" + str(bound))
 
-	print confs	
-
 	for conf in confs:
 		print "start to predict the RI distribution for ", conf
 
@@ -271,29 +363,40 @@ def checkSingleBench(bench, path, numOfSymBounds, symBoundValueForPred):
 		for boundValue in boundValueForPredStr:
 			if (boundValue != ""):
 				boundValueForPred.append(int(boundValue))
-
 		print boundValueForPred
 		
-		histogram_predicted = evaluate(boundValueForPred)
-		print histogram_predicted
+		histogram_src_predicted = evaluate(symbolicHistSrc, boundValueForPred, "src")
+		if (0 in histogram_src_predicted.keys()):
+			del histogram_src_predicted[0]
+		print "histogram_src_predicted", histogram_src_predicted
 
-		histogram_trace = readTraceFile(bench, boundValueForPred)
-		print histogram_trace
+		histogram_srcsnk_predicted = evaluate(symbolicHistSrc, boundValueForPred, "srcsnk")
+		if (0 in histogram_srcsnk_predicted.keys()):
+			del histogram_srcsnk_predicted[0]
+		print "histogram_srcsnk_predicted", histogram_srcsnk_predicted
+
+		histogram_trace = readTraceFile(benchName, boundValueForPred)
+		if (0 in histogram_trace.keys()):
+			del histogram_trace[0]
+		print "histogram_trace", histogram_trace
+	
+		plotHistoToCompare(histogram_src_predicted, histogram_srcsnk_predicted, histogram_trace, benchName, conf)
 		
-		plotHistoToCompare(histogram_predicted, histogram_trace, bench, conf)
+		mr_src_predicted = RItoMR(histogram_src_predicted)
+		mr_srcsnk_predicted = RItoMR(histogram_srcsnk_predicted)
+		mr_trace = RItoMR(histogram_trace)
+		plotMRToCompare(mr_src_predicted, mr_srcsnk_predicted, mr_trace, benchName, conf)
 
 	return
 
 if __name__ == "__main__":
 
 	path = "../synResult/all/"
-	benches = os.listdir(path)
 
 	numOfSymBounds = {"cholesky" : 1, "durbin" : 1, "floyd_warshall" : 1, "gemver" : 1, "gesummv" : 1, "lu" : 1, "ludcmp" : 1, "mvt" : 1, "nussinov" : 1, "stencil" : 1, "trisolv" : 1, "trangle" : 2, "adi" : 2, "atax" : 2, "bicg" : 2, "convolution_2d" : 2, "correlation" : 2, "covariance" : 2, "deriche" : 2, "gramschmidt" : 2, "heat_3d" : 2, "jacobi_1d" : 2, "jacobi_2d" : 2, "seidel_2d" : 2, "symm" : 2, "syr2d" : 2, "syrk" : 2, "trmm" : 2, "convolution_3d" : 3, "doitgen" : 3, "fdtd_2d" : 3, "gemm" : 3, "2mm" : 4, "3mm" : 5}
 
 	symBoundValueForPred = [64, 128, 256]  
-
-	for bench in benches:
-		name = bench.replace(".txt", "")
-		if (name == "floyd_warshall" or name == "gemver" or name == "gesummv"):
-			checkSingleBench(name, path+bench, numOfSymBounds[name], symBoundValueForPred)
+	
+	for benchName in numOfSymBounds.keys():
+		#if (benchName == "stencil"):
+		checkSingleBench(benchName, path, numOfSymBounds[benchName], symBoundValueForPred)
