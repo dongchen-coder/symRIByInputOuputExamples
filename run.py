45e4e89a0d3d7a94d23a5751b24cf3f8080b5b0b
dongchen-coder <jameschennerd@gmail.com>
2020-08-25 16:14:08 +0800

    update run.py

--- run.py
+++ run.py
@@ -12,8 +12,8 @@ if __name__ == '__main__':
 	benches = ["stencil", "2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm", "convolution_2d", "convolution_3d", "trangle"]
 	
 	for bench in benches:
-		os.system("rm -r -f ./inputoutput/ris_per_iter_refsrc/" + bench)
-		os.system("mkdir -p ./inputoutput/ris_per_iter_refsrc/" + bench)
+		os.system("rm -r -f ./synResult/" + bench)
+		os.system("mkdir -p ./synResult/" + bench)
 
 	files = []
 	for bench in benches:
