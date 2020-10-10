ee53669f389a15d0f14a81db2e18edcda462bc21
dongchen-coder <jameschennerd@gmail.com>
2020-10-10 15:54:55 +0800

    update result summarizing script

--- synResult/summarizeResults.py
+++ synResult/summarizeResults.py
@@ -1,67 +1,53 @@
 import os
 
+def summerizeForBenchWithFormat(bench, inOutFormat):
+    files = os.listdir("./" + bench)
+    outputFile = open("./all/" + bench + inOutFormat, "w")
+    
+    for f in files:
+        name = f
+        if (inOutFormat not in name):
+            continue
+        
+        f = open("./"+bench+"/"+f, 'r')
+        for line in f:
+            if ("(^0^)" in line):
+                prog = line.replace("Searched Program (^0^) :", "Prog:")
+                outputFile.write(name + " " + prog)
+        f.close()
+    outputFile.close()
+
 def summerizeForBench(bench):
-	print "Start to summerize " + bench
- 
-	if (not os.path.isdir("./" + bench)):
-		return
+    print "Start to summerize " + bench
+    if (not os.path.isdir("./" + bench)):
+        return
+    files = os.listdir("./" + bench)
+    
+    summerizeForBenchWithFormat(bench, ".RI.ELM.ris_refsrc_Isrc_Psrc")
+    summerizeForBenchWithFormat(bench, ".RI.CLS32_DS8.ris_refsrc_Isrc_Psrc")
+    summerizeForBenchWithFormat(bench, ".RI.ELM.ris_refsrc_Isrc_Psrc_refsnk")
+    summerizeForBenchWithFormat(bench, ".RI.CLS32_DS8.ris_refsrc_Isrc_Psrc_refsnk")
+    summerizeForBenchWithFormat(bench, ".RI.ELM.ris_refsrc_Isrc_Psrc_refsnk_Isnk_Psnk")
+    summerizeForBenchWithFormat(bench, ".RI.CLS32_DS8.ris_refsrc_Isrc_Psrc_refsnk_Isnk_Psnk")
+    IsnkCnt = 0
+    exsitFlag = True
+    while(exsitFlag):
+        exsitFlag = False
+        for f in files:
+            if (".Isnk"+str(IsnkCnt) in f):
+                exsitFlag = True
+                break
+        if (exsitFlag == False):
+            break
+        summerizeForBenchWithFormat(bench, ".Isnk"+str(IsnkCnt)+".ELM.ris_refsrc_Isrc_Psrc_refsnk_Isnk_Psnk")
+        summerizeForBenchWithFormat(bench, ".Isnk"+str(IsnkCnt)+".CLS32_DS8.ris_refsrc_Isrc_Psrc_refsnk_Isnk_Psnk")
+        IsnkCnt += 1
 
-	files = os.listdir("./" + bench)
-
-	outputFile = open("./all/" + bench + "_src.txt", "w")
-	for f in files:
-		name = f
-		if ("_src_" not in name):
-			continue
-		if ("min" in name or "max" in name):
-			continue
-
-		f = open("./"+bench+"/"+f, 'r')
-		for line in f:
-			if ("(^0^)" in line):
-				prog = line.replace("Searched Program (^0^) :", "Prog:")
-				outputFile.write(name + " " + prog)
-		f.close()
-	outputFile.close()
-
-	outputFile = open("./all/" + bench + "_srcsnk.txt", "w")
-	for f in files:
-		name = f
-		if ("_srcsnk_" not in name):
-			continue
-		if ("min" in name or "max" in name):
-			continue
-
-		f = open("./"+bench+"/"+f, 'r')
-		for line in f:
-			if ("(^0^)" in line):
-				prog = line.replace("Searched Program (^0^) :", "Prog:")
-				outputFile.write(name + " " + prog)
-		f.close()
-	outputFile.close()
-
-	outputFile = open("./all/" + bench + "_IBound_result.txt", "w")
-	for f in files:
-		name = f
-		if ("min" not in name and "max" not in name):
-			continue
-		
-		f = open("./"+bench+"/"+f, 'r')
-		for line in f:
-			if ("(^0^)" in line):
-				prog = line.replace("Searched Program (^0^) :", "Prog:")
-				conf_raw = name.replace("_result.txt", "")
-				conf_raw = conf_raw.replace(bench+"_", "")
-
-				outputFile.write(conf_raw + " " + prog)
-		f.close()
-	outputFile.close()
-
-	return
+    return
 
 benches = ["stencil", "2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm", "convolution_2d", "convolution_3d", "trangle"]
 
 #benches = ["stencil"]
 
 for bench in benches:
-	summerizeForBench(bench)
+    summerizeForBench(bench)
