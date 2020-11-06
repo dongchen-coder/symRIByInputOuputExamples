dea87d2782930e10ce2d871063f1d53f69d42763
ictzp <zhaopeng@ict.ac.cn>
2020-11-06 16:00:13 +0800

    update running script

--- run.py
+++ run.py
@@ -1,25 +1,55 @@
 from multiprocessing import Pool
+import logging
+import threading
+import shlex
+import subprocess
 import os
 
 def processFile(f):
-	print f
-	name = f[1].replace(".txt","")
-	os.system("./bin/symRiSymthesiser -FILE ./inputoutput/ris_per_iter_refsrc/" + f[0] +  "/" + f[1] + " -CONSTANTSPRED 1 2 -INTOPSTERM VAR NUM PLUS TIMES MINUS -MODE PerSrcIter -SEARCHTIMEFORTERMSINSECONDS 40 -SEARCHTIMEFORPREDSINSECONDS 80 > ./synResult/" + f[0] + "/" + name + "_src_result.txt")
-	return
+    name = f[2].replace(".txt","")
+    if (f[0] == "IMinMax"):
+        #os.system("./bin/symRiSymthesiser -FILE ./inputoutput/" + f[0] + "/" + f[1] +  "/" + f[2] + " -CONSTANTSPRED 1 2 -INTOPSTERM VAR NUM PLUS TIMES MINUS -MODE PerSrcIter -SEARCHTIMEFORTERMSINSECONDS 40 -SEARCHTIMEFORPREDSINSECONDS 80 > ./synResult/" + f[1] + "/" + name + "_IBound_result.txt")
+        output = ""
+        cmd = "./bin/symRiSymthesiser -FILE ./inputoutput/" + f[0] + "/" + f[1] +  "/" + f[2] + " -CONSTANTSPRED 1 2 -INTOPSTERM VAR NUM PLUS TIMES MINUS -MODE PerSrcIter -SEARCHTIMEFORTERMSINSECONDS 40 -SEARCHTIMEFORPREDSINSECON    DS 80"
+        process = subprocess.Popen([cmd], stdout = subprocess.PIPE, shell=True)
+        output = [f[1] + "/" + name, process.stdout.read()]
+    if (f[0] == "ris_per_iter_refsrc"):
+        print "Start", f[3]
+        cmd = "./bin/symRiSymthesiser -FILE ./inputoutput/" + f[0] + "/" + f[1] +  "/" + f[2] + " -CONSTANTSPRED 1 2 -INTOPSTERM VAR NUM PLUS TIMES MINUS -MODE PerSrcIter -SEARCHTIMEFORTERMSINSECONDS 40 -SEARCHTIMEFORPREDSINSECONDS 80"
+        #print cmd
+        os.system(cmd)
+        print "End", f[3]
+        
+        '''
+        output = ""
+        cmd = "./bin/symRiSymthesiser -FILE ./inputoutput/" + f[0] + "/" + f[1] +  "/" + f[2] + " -CONSTANTSPRED 1 2 -INTOPSTERM VAR NUM PLUS TIMES MINUS -MODE PerSrcIter -SEARCHTIMEFORTERMSINSECONDS 40 -SEARCHTIMEFORPREDSINSECONDS 80"
+        process = subprocess.Popen([cmd], stdout = subprocess.PIPE, shell=True)
+        output = [f[1] + "/" + name, process.stdout.read()]
+        '''
+    return
 
 if __name__ == '__main__':
 
-	benches = ["stencil", "2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm", "convolution_2d", "convolution_3d", "trangle"]
-	
-	for bench in benches:
-		os.system("rm -r -f ./synResult/" + bench)
-		os.system("mkdir -p ./synResult/" + bench)
+    #benches = ["stencil", "2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm", "convolution_2d", "convolution_3d", "trangle"]
+    benches = ["stencil"]
 
-	files = []
-	for bench in benches:
-		filesForBench = os.listdir("./inputoutput/ris_per_iter_refsrc/" + bench)
-		files += [[bench, f] for f in filesForBench]
+    for bench in benches:
+        os.system("rm -r -f ./synResult/" + bench)
+        os.system("mkdir -p ./synResult/" + bench)
 
-	p = Pool(2)
-	p.map(processFile, files)
-	
+    files = []
+    
+    for bench in benches:
+        filesForBench = os.listdir("./inputoutput/ris_per_iter_refsrc/" + bench)
+        files += [["ris_per_iter_refsrc", bench, f, benches.index(bench)] for f in filesForBench]
+    '''
+    for bench in benches:
+        filesForBench = os.listdir("./inputoutput/IMinMax/" + bench)
+        for f in filesForBench:
+            if ("snk" not in f):
+                files.append(["IMinMax",bench, f])
+    '''
+    p = Pool(60)
+    p.map(processFile, files)
+    p.close()
+    p.join()
--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@ NPROC=0
 #durbin floyd_warshall gemver gesummv lu ludcmp mvt nussinov trisolv stencil cholesky
 #trangle adi atax bicg convolution_2d correlation covariance deriche gramschmidt jacobi_1d jacobi_2d seidel_2d symm syr2d syrk trmm heat_3d
 #for bench in cholesky durbin floyd_warshall gemver gesummv lu ludcmp mvt nussinov stencil trisolv stencil
-for bench in stencil #2mm 3mm adi atax bicg cholesky correlation covariance deriche doitgen durbin fdtd_2d floyd_warshall gemm gemver gesummv gramschmidt heat_3d jacobi_1d jacobi_2d lu ludcmp mvt nussinov seidel_2d symm syr2d syrk trisolv trmm convolution_2d convolution_3d trangle
+#2mm 3mm adi atax bicg cholesky correlation covariance deriche doitgen durbin fdtd_2d floyd_warshall gemm gemver gesummv gramschmidt heat_3d jacobi_1d jacobi_2d lu ludcmp mvt nussinov seidel_2d symm syr2d syrk trisolv trmm convolution_2d convolution_3d trangle
+for bench in 2mm 3mm adi atax bicg cholesky correlation covariance deriche durbin fdtd_2d floyd_warshall gemm gemver gesummv gramschmidt jacobi_1d jacobi_2d lu ludcmp mvt nussinov seidel_2d symm syr2d syrk trisolv trmm convolution_2d convolution_3d
 do
 
     rm -r -f ./synResult/${bench}
@@ -12,10 +13,22 @@ do
         for entry in "./inputoutput/"${dir}/${bench}/*
 	    do
 		    name=${entry#./inputoutput/${dir}/${bench}/}
-		    #name=${name%.txt}
 		    echo $name
-        
-            ./bin/symRiSymthesiser -FILE ${entry} -CONSTANTSPRED 1 2 -CONSTANTSTERM 0 1 2 3 4 5 6 -INTOPSTERM VAR NUM PLUS TIMES MINUS -SEARCHTIMEFORTERMSINSECONDS 80 -SEARCHTIMEFORPREDSINSECONDS 80 > ./synResult/${bench}/${name}.${dir} &
+             
+            if [ ${dir} == "ris_refsrc_Isrc_Psrc" ]; then
+                echo "SrcOnly"
+                ./bin/symRiSymthesiser -FILE ${entry} -CONSTANTSPRED 1 2 -CONSTANTSTERM 0 1 2 3 4 5 6 -INTOPSTERM VAR NUM PLUS TIMES MINUS -SEARCHTIMEFORTERMSINSECONDS 80 -SEARCHTIMEFORPREDSINSECONDS 80 -RULESTOAPPLY SrcOnly > ./synResult/${bench}/${name}.${dir} &
+            elif [ ${dir} == "ris_refsrc_Isrc_Psrc_refsnk" ]; then
+                ./bin/symRiSymthesiser -FILE ${entry} -CONSTANTSPRED 1 2 -CONSTANTSTERM 0 1 2 3 4 5 6 -INTOPSTERM VAR NUM PLUS TIMES MINUS -SEARCHTIMEFORTERMSINSECONDS 80 -SEARCHTIMEFORPREDSINSECONDS 80 -RULESTOAPPLY SrcEnhanced > ./synResult/${bench}/${name}.${dir} &
+            #elif [ ${dir} == "ris_refsrc_Isrc_Psrc_refsnk_Isnk_Psnk" ]; then   
+                #./bin/symRiSymthesiser -FILE ${entry} -CONSTANTSPRED 1 2 -CONSTANTSTERM 0 1 2 3 4 5 6 -INTOPSTERM VAR NUM PLUS TIMES MINUS -SEARCHTIMEFORTERMSINSECONDS 80 -SEARCHTIMEFORPREDSINSECONDS 80 -RULESTOAPPLY SrcSnk > ./synResult/${bench}/${name}.${dir} &
+            elif [ ${dir} == "ris_Ibound" ]; then
+                ./bin/symRiSymthesiser -FILE ${entry} -CONSTANTSPRED 1 2 -CONSTANTSTERM 0 1 2 3 4 5 6 -INTOPSTERM VAR NUM PLUS TIMES MINUS -SEARCHTIMEFORTERMSINSECONDS 80 -SEARCHTIMEFORPREDSINSECONDS 80 -RULESTOAPPLY SrcOnly > ./synResult/${bench}/${name}.${dir} &
+            else
+                echo "INV"
+            fi
+
+            #./bin/symRiSymthesiser -FILE ${entry} -CONSTANTSPRED 1 2 -CONSTANTSTERM 0 1 2 3 4 5 6 -INTOPSTERM VAR NUM PLUS TIMES MINUS -SEARCHTIMEFORTERMSINSECONDS 80 -SEARCHTIMEFORPREDSINSECONDS 80 > ./synResult/${bench}/${name}.${dir} &
 
 		    NPROC=$(($NPROC+1))
 		    if [ "$NPROC" -ge 60 ]; then
@@ -24,20 +37,4 @@ do
 		    fi
 	    done
     done
-    '''
-	for entry in "./inputoutput/ris_per_iter_refsrcsnk"/${bench}/*
-    do
-        name=${entry#./inputoutput/ris_per_iter_refsrcsnk/${bench}/}
-		name=${name%.txt}
-		echo $name
-		
-        ./bin/symRiSymthesiser -FILE ./inputoutput/ris_per_iter_refsrcsnk/${bench}/${name}.txt -CONSTANTSPRED 1 2 -INTOPSTERM VAR NUM PLUS TIMES MINUS -MODE PerSrcSnk -SEARCHTIMEFORTERMSINSECONDS 40 -SEARCHTIMEFORPREDSINSECONDS 4000 > ./synResult/${bench}/${name}_srcsnk_result.txt &
-
-		NPROC=$(($NPROC+1))
-		if [ "$NPROC" -ge 60 ]; then
-			wait
-			NPROC=0
-		fi
-    done
-    '''
 done
