79a00f713f2bc767775618368191eac444b52533
dongchen-coder <jameschennerd@gmail.com>
2020-11-08 14:58:33 +0800

    update rt.h

--- bench/utility/rt.h
+++ bench/utility/rt.h
@@ -9,6 +9,7 @@ using namespace std;
 //#define DS 8
 //#define TRACE
 //#define PROFILE
+#define PROGILE_IBOUND
 
 uint64_t refT = 0;
 
@@ -32,6 +33,18 @@ void rtTmpAccess(uint64_t addr, uint64_t ref_id, uint64_t array_id, vector<int> 
 	addr = addr * DS / CLS;
     refT++;
     
+#ifdef PROFILE_IBOUND
+    cout << ref_id << ",";
+    for (int i = 0; i < idx.size(); i++) {
+        cout << idx[i];
+        if (i != idx.size() - 1) {
+            cout << " ";
+        } else {
+            cout << endl;
+        }
+    }
+#endif
+
     if (lat.find(addr) != lat.end()) {
         uint64_t ri = refT - lat[addr];
 
