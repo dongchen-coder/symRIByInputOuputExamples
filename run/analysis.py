7d1c7c8d7d793bead936dcb3c9287584f79ff140
dongchen-coder <jameschennerd@gmail.com>
2021-07-12 23:36:08 +0800

    add analysis.py to figure out the input-output examples that failed to find a sym_ri

--- run/analysis.py
+++ run/analysis.py
@@ -1,23 +1,36 @@
 import os
 
-ioe_path = "./data/input-output_examples/"
-sym_ri_path = "./data/sym_ri/"
-
-benches = ["2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm"]
-
-
-for cache_config in ["elm", "clS32_ds8"]:
+def get_failed_ioe_with_no_sym_ri(cache_config, benches, ioe_path, sym_ri_path):
+    failed_ioe = {}
     for mode in ["src_only", "src_enhanced", "src_snk"]:
-        print cache_config, mode, "************************************"
+        print cache_config, mode, "------------------------"
+        failed_ioe[mode] = {}
         for bench in benches:
-            ios_files = []
+            ioe_files = []
             sym_ri_files = []
             if (os.path.exists(ioe_path + bench + '/' + mode)):
-                ios_files = os.listdir(ioe_path + bench + '/' + mode)
+                ioe_files = os.listdir(ioe_path + bench + '/' + mode)
             if (os.path.exists(sym_ri_path + bench)):
                 sym_ri_files = os.listdir(sym_ri_path + bench)
-            #ios_files = [x[0:x.index('.')] for x in ios_files]        
-            sym_ri_files = [x[0:-len(mode)] for x in sym_ri_files]
-            print sym_ri_files
+            
+            failed_ioe_files = []
+            for sym_ri_file in sym_ri_files:
+                if (mode in sym_ri_file):
+                    f = open(sym_ri_path + bench + "/" + sym_ri_file)
+                    first_line = f.readline()
+                    if ("(^0^)" not in first_line):
+                        failed_ioe_files.append(sym_ri_file)
+            failed_ioe[mode][bench] = failed_ioe_files
+            print bench, len(ioe_files), len(failed_ioe_files),
+        print ""
+    return failed_ioe
+    
+if __name__ == "__main__":
+    
+    benches = ["2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm"]
 
+    ioe_path = "./data/input-output_examples/"
+    sym_ri_path = "./data/sym_ri/"
 
+    failed_ioe = get_failed_ioe_with_no_sym_ri("clS32_ds8", benches, ioe_path, sym_ri_path)
+    
--- run/gen_input-output_examples.py
+++ run/gen_input-output_examples.py
@@ -8,7 +8,8 @@ from sample_raw_data import *
 if __name__ ==  "__main__":
     train_sizes = [4, 6, 8, 12, 20]
     
-    bench_n_paras = {"stencil" : 1} #"stencil_tiled" : 2, "cholesky" : 1, "durbin" : 1, "floyd_warshall" : 1, "gemver" : 1, "gesummv" : 1, "lu" : 1, "ludcmp" : 1, "mvt" : 1, "nussinov" : 1, "stencil" : 1, "trisolv" : 1, 'stencil_tiled' : 2, "heat_3d" : 2, "trangle" : 2, "adi" : 2, "atax" : 2, "bicg" : 2, "convolution_2d" : 2, "correlation" : 2, "covariance" : 2, "deriche" : 2, "gramschmidt" : 2, "heat_3d" : 2, "jacobi_1d" : 2, "jacobi_2d" : 2, "seidel_2d" : 2, "symm" : 2, "syr2d" : 2, "syrk" : 2, "trmm" : 2, "doitgen" : 3, "convolution_3d" : 3, "doitgen" : 3, "fdtd_2d" : 3, "gemm" : 3, "2mm" : 4, "3mm" : 5}
+    bench_n_paras = {"correlation" : 2}
+    #bench_n_paras = {"stencil" : 1, "stencil_tiled" : 2, "cholesky" : 1, "durbin" : 1, "floyd_warshall" : 1, "gemver" : 1, "gesummv" : 1, "lu" : 1, "ludcmp" : 1, "mvt" : 1, "nussinov" : 1, "stencil" : 1, "trisolv" : 1, 'stencil_tiled' : 2, "heat_3d" : 2, "trangle" : 2, "adi" : 2, "atax" : 2, "bicg" : 2, "convolution_2d" : 2, "correlation" : 2, "covariance" : 2, "deriche" : 2, "gramschmidt" : 2, "heat_3d" : 2, "jacobi_1d" : 2, "jacobi_2d" : 2, "seidel_2d" : 2, "symm" : 2, "syr2d" : 2, "syrk" : 2, "trmm" : 2, "doitgen" : 3, "convolution_3d" : 3, "doitgen" : 3, "fdtd_2d" : 3, "gemm" : 3, "2mm" : 4, "3mm" : 5}
 
     # gen input-output examples for all benchmarks
     for bench, n_paras in bench_n_paras.items():
