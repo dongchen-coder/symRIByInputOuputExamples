1590861f452bd7757f2233ec51e3630817b79f9c
dongchen-coder <jameschennerd@gmail.com>
2021-07-13 13:56:31 +0800

    add run failed ioe funtion

--- run/analysis.py
+++ run/analysis.py
@@ -1,10 +1,10 @@
 import os
 
 def get_failed_ioe_with_no_sym_ri(cache_config, benches, ioe_path, sym_ri_path):
-    failed_ioe = {}
-    for mode in ["src_only", "src_enhanced", "src_snk"]:
+    failed_ioes = {}
+    for mode in ["src_only", "src_enhanced", "src_snk", "ibound"]:
         print cache_config, mode, "------------------------"
-        failed_ioe[mode] = {}
+        failed_ioes[mode] = {}
         for bench in benches:
             ioe_files = []
             sym_ri_files = []
@@ -20,11 +20,29 @@ def get_failed_ioe_with_no_sym_ri(cache_config, benches, ioe_path, sym_ri_path):
                     first_line = f.readline()
                     if ("(^0^)" not in first_line):
                         failed_ioe_files.append(sym_ri_file)
-            failed_ioe[mode][bench] = failed_ioe_files
+            failed_ioes[mode][bench] = failed_ioe_files
             print bench, len(ioe_files), len(failed_ioe_files),
         print ""
-    return failed_ioe
-    
+    return failed_ioes
+
+def run_failed_ioes(cache_config, failed_ioes, ioe_path, sym_ri_path):
+    for mode in failed_ioes.keys():
+        for bench in failed_ioes[mode].keys():
+            for file_name in failed_ioes[mode][bench]:
+                ioe_file = ioe_path + bench + '/' + mode + '/' + file_name.replace("."+mode,"")
+                synthesizer_conf = " -CONSTANTSPRED 1 2 -CONSTANTSTERM 0 1 2 3 4 5 6 -INTOPSTERM VAR NUM PLUS TIMES MINUS -SEARCHTIMEFORTERMSINSECONDS 5 -SEARCHTIMEFORPREDSINSECONDS 5 "
+                cmd = ""
+                if mode == "src_only" or mode == "ibound":
+                    cmd = "../synthesizer/bin/symRiSymthesiser -FILE "+ ioe_file + synthesizer_conf + "-RULESTOAPPLY SrcOnly > " + sym_ri_path+bench+"/"+file_name
+                if mode == "src_enhanced":
+                    cmd = "../synthesizer/bin/symRiSymthesiser -FILE "+ ioe_file + synthesizer_conf + "-RULESTOAPPLY SrcEnhanced > " + sym_ri_path+bench+"/"+file_name
+                if mode == "src_snk":
+                    cmd = "../synthesizer/bin/symRiSymthesiser -FILE "+ ioe_file + synthesizer_conf + "-RULESTOAPPLY SrcSnk > " + sym_ri_path+bench+"/"+file_name
+                print cmd
+                os.system(cmd)
+                exit()
+    return
+
 if __name__ == "__main__":
     
     benches = ["2mm", "3mm", "adi", "atax", "bicg", "cholesky", "correlation", "covariance", "deriche", "doitgen", "durbin", "fdtd_2d", "floyd_warshall", "gemm", "gemver", "gesummv", "gramschmidt", "heat_3d", "jacobi_1d", "jacobi_2d", "lu", "ludcmp", "mvt", "nussinov", "seidel_2d", "symm", "syr2d", "syrk", "trisolv", "trmm"]
@@ -32,5 +50,5 @@ if __name__ == "__main__":
     ioe_path = "./data/input-output_examples/"
     sym_ri_path = "./data/sym_ri/"
 
-    failed_ioe = get_failed_ioe_with_no_sym_ri("clS32_ds8", benches, ioe_path, sym_ri_path)
-    
+    failed_ioes = get_failed_ioe_with_no_sym_ri("cls32_ds8", benches, ioe_path, sym_ri_path)
+    run_failed_ioes("cls32_dc8", failed_ioes, ioe_path, sym_ri_path)
