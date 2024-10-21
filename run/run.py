6ea4df2c170aa7bf9b6b3ee94b9872b40a8e3297
Dong Chen <jameschennerd@gmail.com>
2024-10-21 11:36:18 +0800

    config for next run

--- run/run.py
+++ run/run.py
@@ -9,9 +9,9 @@ from summarize_sym_ri import summerize_sym_ri
 syn_config = {
     "constants_for_predicate" : " -CONSTANTSPRED 1 2",
     "constants_for_term" : " -CONSTANTSTERM 0 1 2 3 4 5 6 7",
-    "int_ops_for_term" : " -INTOPSTERM VAR NUM PLUS TIMES MINUS DIV8",
-    "search_time_for_terms_in_seconds" : " -SEARCHTIMEFORTERMSINSECONDS 120",
-    "search_time_for_preds_in_seconds" : " -SEARCHTIMEFORPREDSINSECONDS 120",
+    "int_ops_for_term" : " -INTOPSTERM VAR NUM PLUS TIMES MINUS DIV",
+    "search_time_for_terms_in_seconds" : " -SEARCHTIMEFORTERMSINSECONDS 240",
+    "search_time_for_preds_in_seconds" : " -SEARCHTIMEFORPREDSINSECONDS 240",
     "rules_to_apply" : " -RULESTOAPPLY SrcOnly"
 }
 
@@ -40,10 +40,10 @@ if __name__ == "__main__":
     
     all_bench_n_parms = poly_bench_n_parms
     train_sizes = [8, 12, 16, 20, 32]
-    verify_sizes = [32]
+    verify_sizes = [32, 64]
 
     for bench, n_parms in all_bench_n_parms.items():
         gen_trace([bench, n_parms], verify_sizes, "cls32_ds8")
-        #gen_input_output_examples([bench, n_parms], train_sizes, num_of_samples)
-        #gen_sym_ri(bench, syn_config, num_of_cpus)
-        #summerize_sym_ri(bench, "cls32_ds8")
+        gen_input_output_examples([bench, n_parms], train_sizes, num_of_samples)
+        gen_sym_ri(bench, syn_config, num_of_cpus)
+        summerize_sym_ri(bench, "cls32_ds8")
